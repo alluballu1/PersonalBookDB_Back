7d68a4a52d350bdde46869d1d6db62c6fa255d6d
alluballu1 <45081751+alluballu1@users.noreply.github.com>
2022-02-12 19:51:37 +0200

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -4,31 +4,35 @@ const bcrypt = require("bcryptjs");
 const { User } = require("../models");
 
 loginRouter.post("/", async (request, response) => {
-  const body = request.body;
-  const user = await User.findAll({ where: { name: body.name } });
-  const passwordCorrect =
-    user.length === 0
-      ? false
-      : await bcrypt.compare(body.password, user[0].dataValues.passwordHash);
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: "invalid username or password",
-    });
+  try {
+    const body = request.body;
+    console.log(body);
+    const user = await User.findAll({ where: { name: body.name } });
+    const passwordCorrect =
+      user.length === 0
+        ? false
+        : await bcrypt.compare(body.password, user[0].dataValues.passwordHash);
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({
+        error: "invalid username or password",
+      });
+    }
+
+    console.log(user[0].dataValues);
+
+    const userForToken = {
+      name: user[0].dataValues.name,
+      userId: user[0].dataValues.userId,
+    };
+    const token = jwt.sign(userForToken, process.env.SECRET);
+
+    console.log(jwt.verify(token, process.env.SECRET));
+
+    const { passwordHash, createdAt, updatedAt, ...rest } = user[0].dataValues;
+    response.status(200).send({ token, name: user.name, user: rest });
+  } catch (error) {
+    response.json("Invalid credentials").status(500);
   }
-
-  console.log(user[0].dataValues)
-
-
-  const userForToken = {
-    name: user[0].dataValues.name,
-    userId: user[0].dataValues.userId
-  };
-  const token = jwt.sign(userForToken, process.env.SECRET);
-
-  console.log( jwt.verify(token, process.env.SECRET))
-
-  const { passwordHash, createdAt, updatedAt, ...rest } = user[0].dataValues;
-  response.status(200).send({ token, name: user.name, user: rest });
 });
 
 module.exports = loginRouter;
